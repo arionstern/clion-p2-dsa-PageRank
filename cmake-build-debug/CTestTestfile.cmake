73805350fcb413ac45f1982d334b52834383b1db
arionstern <aristern06@gmail.com>
2026-01-06 21:03:33 -0500

    Initial Commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/arist/CLionProjects/clion-p2-dsa
+# Build directory: C:/Users/arist/CLionProjects/clion-p2-dsa/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/arist/CLionProjects/clion-p2-dsa/cmake-build-debug/Tests-b12d07c_include.cmake")
+subdirs("_deps/catch2-build")
